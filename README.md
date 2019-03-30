1c7d1cb2fc3221480905b56dd23067dc7c993442
slimish <shadi.abdelfatah@gmail.com>
2019-03-30 16:07:10 +0300

    statsd stdout logger

---
+++ README.md
@@ -0,0 +1,17 @@
+### golstatsd
+
+Statsd **log**ger written in Golang that reads the udp packets sent from a statsd client
+and write them to stdout, by default it listens to port 8125 on localhost.
+
+to install it:
+
+`go get go get github.com/shadi/golstatsd`
+
+and run it using:
+`golstatsd`
+
+or if the client is sending the metrics to a different port:
+`golstatsd -port $PORT`
+
+to write metrics to a file and run the process in the background:
+`golstatsd &> $LOG_FILE &`
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+)
+
+func main() {
+
+	port := flag.Int("port", 8125, "Statsd ports")
+
+	flag.Parse()
+
+	addr := net.UDPAddr{
+		Port: *port,
+		IP: net.ParseIP("127.0.0.1"),
+	}
+
+	conn, err := net.ListenUDP("udp", &addr)
+	defer conn.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	var buf [1024]byte
+	for {
+		respLen, _, err := conn.ReadFromUDP(buf[:])
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(buf[:respLen]))
+	}
+}
